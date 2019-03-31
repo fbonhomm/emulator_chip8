84e48b56113631258b2ac8f025620a90bb6f8317
florian <florian-bonhomme@outlook.fr>
2019-03-31 18:24:07 +0200

    beginning implement cpu

--- src/cpu.go
+++ src/cpu.go
@@ -1,6 +1,35 @@
 
 package cpu
 
-type feature struct {
-    uint8 memory[4096]
+
+const SIZE_MEMORY uint16 = 4096 // taille de la memoire total
+const START_MEMORY uint16 = 512 // adresse du debut de la memoire
+const NB_REGISTER uint16 = 16 // nombre total de registres
+const NB_LEVEL_STACK uint16 = 16 // nombre de niveaux pour la stack
+
+
+type Feature struct {
+    memory[SIZE_MEMORY] uint8, // tableaux qui represente la memoire
+    register[NB_REGISTER] uint8, //  tableaux qui represente les registres V{0..F}
+    stack[NB_LEVEL_STACK] uint16, // tableaux qui represente la stack de sauvegarde
+    index_memory uint16, // represente la tete de lecture de la memoire
+    index_stack uint8, // represente la tete de lecture de la stack
+    system_timer uint8, // mimuterie systeme
+    sound_timer uint8, // mimuterie sonore
+    address_stock uint16, // stocke une adresse memoire
+}
+
+// define getter index_memory
+func (f *Feature) get() (uint16) {
+  return f.index_memory
+}
+
+// decremente les timers
+func (f *Feature) decrease() (uint16) {
+  if f.system_timer > 0 {
+    f.system_timer -= 1
+  }
+  if f.sound_timer > 0 {
+    f.sound_timer -= 1
+  }
 }
--- src/display.go
+++ src/display.go
@@ -1,11 +1,22 @@
-package main
+package display
 
 import (
-	"fmt"
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 )
 
+const PIXEL_SIZE unit8 = 8
+const PIXEL_BLACK unit8 = 0
+const PIXEL_WHITE unit8 = 1
+const HEIGHT unit8 = 32
+const WIDTH unit8 = 64
+const WINDOW_HEIGHT unit16 = HEIGHT * PIXEL_SIZE
+const WINDOW_WIDTH unit16 = WIDTH * PIXEL_SIZE
+
+
+type Pixel struct {
+  color uint8, // couleur du pixel
+}
 
 func run() {
   cfg := pixelgl.WindowConfig{
