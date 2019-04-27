49a8704a1634e55273c3e83ea8c93f5a38194cf8
florian <florian-bonhomme@outlook.fr>
2019-04-27 13:45:48 +0200

    test sdl for display, good

--- src/display.go
+++ src/display.go
@@ -1,69 +1,85 @@
 package main
 
 import (
-	"image/color"
-
-	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/veandco/go-sdl2/sdl"
 )
 
-const pixelSize uint8 = 16
-const height uint8 = 32
-const width uint8 = 64
-const windowHeight uint16 = uint16(height) * uint16(pixelSize)
-const windowWidth uint16 = uint16(width) * uint16(pixelSize)
+const pixelSize uint32 = 16
+const height uint32 = 32
+const width uint32 = 64
+const windowHeight uint32 = height * pixelSize
+const windowWidth uint32 = width * pixelSize
+const white uint32 = 0xffffffff
+const black uint32 = 0x00000000
 
 type elementaryBlock struct {
-	opts  *ebiten.DrawImageOptions
-	color color.Color
+	color uint32
 }
 
 type screen struct {
 	display [width][height]elementaryBlock
-	x       int
-	y       int
-}
-
-func (s *screen) update(window *ebiten.Image) error {
-	// window.Fill(color.Black)
-	window.Fill(color.NRGBA{0xff, 0x00, 0x00, 0xff})
-
-	if ebiten.IsDrawingSkipped() {
-		return nil
-	}
-
-	ebitenutil.DebugPrint(screen, "Our first game in Ebiten!")
-	s.x++
-	s.y++
-	s.drawPixel(x, y, window)
-	return nil
+	window  *sdl.Window
+	surface *sdl.Surface
 }
 
 func (s *screen) initialize() {
-	for ; x < (x + pixelSize); x++ {
-		for ; y < (y + pixelSize); y++ {
-			s.display[x][y].opts.GeoM.Translate(x*pixelSize, y*pixelSize)
-			s.display[x][y].color = color.Black
-		}
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
 	}
+	defer sdl.Quit()
 
-	err := ebiten.Run(s.update, windowWidth, windowHeight, 1, "Emulator Chip8")
+	window, err := sdl.CreateWindow(
+		"Emulator Chip8",
+		sdl.WINDOWPOS_UNDEFINED,
+		sdl.WINDOWPOS_UNDEFINED,
+		int32(windowWidth), int32(windowHeight),
+		sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
 	}
+	defer window.Destroy()
+
+	surface, err := window.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+
+	s.window = window
+	s.surface = surface
+	s.surface.FillRect(nil, black)
+	s.window.UpdateSurface()
+
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				running = false
+				break
+			}
+		}
+	}
 }
 
-func (s *screen) drawPixel(x, y int, window *ebiten.Image) {
-	var square *ebiten.Image
+func (s *screen) initDisplay() {
+	for x := 0; uint32(x) < width; x++ {
+		for y := 0; uint32(y) < height; y++ {
+			s.display[x][y].color = black
+		}
+	}
+}
 
-	square, _ = ebiten.NewImage(pixelSize, pixelSize, ebiten.FilterNearest)
+func (s *screen) printPixel(x, y uint32) {
+	s.surface.FillRect(nil, 0)
 
-	square.fill(color.White)
-	window.DrawImage(square, s.display[x][y].opts)
+	rect := sdl.Rect{int32(x), int32(y), int32(pixelSize), int32(pixelSize)}
+	s.surface.FillRect(&rect, 0xffff0000)
+	s.window.UpdateSurface()
 }
 
 func main() {
 	var win = screen{}
 
+	// win.initDisplay()
 	win.initialize()
 }
